e5e1751fda9b127ec8b2a5481974cdfe930a76ce
luizPhelipeSantana <luiz.oliveira@sptech.school>
2026-05-20 19:19:44 -0300

    Criado a view para ultimas leituras e conformidade do sistema

--- view/leituras_dash.sql
+++ view/leituras_dash.sql
@@ -1,30 +1,12 @@
-CREATE VIEW vw_leituras_ponto_operacional AS
+CREATE VIEW vw_conformidade_do_sistema AS
 SELECT 
-    po.id_ponto_operacional,
-    po.nome AS nome_ponto_operacional,
-    tpo.tipo AS tipo_ponto_operacional,
-    ae.nome AS ambiente,
-    s.id_sensor,
-    s.identificador AS identificador_sensor,
-    l.id_leitura,
-    l.temperatura,
-    l.data_hora,
-    cpo.temp_min,
-    cpo.temp_max,
-    CASE
-        WHEN l.temperatura < cpo.temp_min THEN 0
-        WHEN l.temperatura > cpo.temp_max THEN 0
-        ELSE 1
-    END AS status_temperatura
-FROM ponto_operacional po
-
-JOIN tipo_ponto_operacional tpo
-    ON po.fk_tipo_po = tpo.id_tipo_po
-JOIN ambiente_externo ae
-    ON po.fk_ambiente = ae.id_ambiente
-JOIN configuracao_ponto_operacional cpo
-    ON po.fk_configuracao_po = cpo.id_configuracao
-JOIN sensor s
-    ON s.fk_po = po.id_ponto_operacional
-JOIN leitura l
-    ON l.fk_sensor = s.id_sensor) as tudo;
+    id_empresa,
+    ROUND(
+        ( SUM(
+			CASE
+				WHEN status_operacional = 'NORMAL'
+				THEN 1
+				ELSE 0
+			END ) * 100.0 ) / COUNT(*), 2) AS porcentagem
+FROM vw_status_ponto_operacional
+GROUP BY id_empresa;
